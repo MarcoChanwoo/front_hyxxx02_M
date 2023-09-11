a925105b767b8fe5b190b268fa0c77a8d717e8dc
Marco <cwhong8087@gmail.com>
2023-09-11 18:22:17 +0900

    2nd commit

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from '../node_modules/react-router-dom/dist/index';
+// import { createStore } from '../node_modules/redux/index';
+import { createStore } from 'redux';
+import rootReducer from './modules/index';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+import { composeWithDevTools } from '../node_modules/redux-devtools-extension/index';
+// import { Provider } from '../node_modules/react-redux/es/exports';
+import { Provider } from 'react-redux';
+
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+        ,
+    </Provider>,
 );
