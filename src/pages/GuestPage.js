1bc2d13a6146675548ad08b8aa981dedeca39b23
Marco <cwhong8087@gmail.com>
2023-09-12 23:58:33 +0900

    guest

--- src/pages/GuestPage.js
+++ src/pages/GuestPage.js
@@ -9,9 +9,17 @@ const GuestPage = ({ onClick }) => {
             <Container />
             <div className="container">
                 <h2>당신의 현장을 안전하게!</h2>
+                <div>
+                    (주)호윤상사는 이미 수많은 회사와 함께해 왔습니다.
+                    <br />
+                    이제 당신도 우리의 능력을 이용하실 수 있습니다.
+                    <br />
+                    궁금하신 사항은 언제든지 아래 버튼을 통해 문의하세요.
+                </div>
             </div>
+            <br />
             <Button to="/write" onClick={onClick}>
-                파일 올리기
+                견적 신청하기
             </Button>
         </>
     );
--- src/pages/WritePage.js
+++ src/pages/WritePage.js
@@ -6,8 +6,17 @@ const WritePage = () => {
     return (
         <div>
             <Header />
+            <h2>견적 요청</h2>
+            <div>
+                귀하의 현장 사진을 보여주세요.
+                <br />
+                (주)호윤상사와 함께 현장 견적을 함께해 드립니다.
+            </div>
+            <br />
             <div>이곳에 파일을 업로드할 수 있습니다.</div>
+            <br />
             <Button>파일 업로드</Button>
+            <br />
             <br />
             <br />
             <Link to="/">첫 화면으로 이동</Link>
